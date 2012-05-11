fbc7157f22ea0cca1f0a9ad54e4d4916d6cecb0e
Josef Ottosson <josef@josf.se>
2012-05-11 14:26:09 +0200

    committing changes to generated .js file

--- jquery.svg.input.js
+++ jquery.svg.input.js
@@ -1,3 +1,11 @@
+Copyright (c) 2012 Tim Brandin and Josef Ottosson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 // Function.inheritsFrom
 Function.prototype.inheritsFrom=function(a){
 	if(a.constructor==Function){
@@ -3108,10 +3116,6 @@ $(document).ready(function(){
 	domReady = true;
 	
 	goReady();
-
-	$('html').css({opacity: 0});
-	
-	$('.svg-container').transition({ x: '-2000px', y: '0', height: ($(window).height()*.6)+'px' }, 0);
 	
 });
 
@@ -3121,8 +3125,6 @@ $(window).load(function () {
 	loaded = true;
 	
 	goReady();
-	
-	$('html').animate({opacity: 1}, 100);
 	
 });
 
